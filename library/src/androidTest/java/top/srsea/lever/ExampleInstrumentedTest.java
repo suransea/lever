30e375702f91c602da883e2222f5d03c94a69715
wanghailun1 <wanghailun1@xiaomi.com>
2019-05-20 14:00:12 +0800

    解决LICENSE.md问题

--- library/src/androidTest/java/top/srsea/lever/ExampleInstrumentedTest.java
+++ library/src/androidTest/java/top/srsea/lever/ExampleInstrumentedTest.java
@@ -2,11 +2,11 @@ package top.srsea.lever;
 
 import android.content.Context;
 
+import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.runner.AndroidJUnit4;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
-import androidx.test.InstrumentationRegistry;
-import androidx.test.runner.AndroidJUnit4;
 
 import static org.junit.Assert.assertEquals;
 
@@ -20,7 +20,7 @@ public class ExampleInstrumentedTest {
     @Test
     public void useAppContext() {
         // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getTargetContext();
+        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
 
         assertEquals("top.srsea.library.test", appContext.getPackageName());
     }
